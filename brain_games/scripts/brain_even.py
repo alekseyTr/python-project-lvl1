2b457e39311b0b2c2999d8c94708512ed25d6eaf
aleksey <tra@jcat.ru>
2021-04-10 14:51:07 +0300

    Add logic game "Even/Odd"

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""Even Odd game."""
+
+import prompt
+from brain_games.game_even_odd import start_game
+
+
+def main():
+    """Welcome user and start game."""
+    print('Welcome to the Brain Games')
+    name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(name))
+
+    start_game(name)
+
+
+if __name__ == '__main__':
+    main()
