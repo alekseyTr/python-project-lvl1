7b8b0c00abedbb97ac987b6706300f5c64a1f399
aleksey <tra@jcat.ru>
2021-04-11 15:43:55 +0300

    Game "Calculator"

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/alekseyTr/python-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/alekseyTr/python-project-lvl1/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/alekseyTr/python-project-lvl1)
+
+## Calculator
+[![asciicast](https://asciinema.org/a/406522.svg)](https://asciinema.org/a/406522)
---
+++ brain_games/games/even_odd.py
@@ -0,0 +1,62 @@
+"""Even odd game."""
+
+import prompt
+from brain_games.functions import is_even
+
+
+def correct_answer(question):
+    """Get correct answer.
+
+    Parameters:
+        question: int
+
+    Returns:
+        list
+
+    """
+    return ['yes', 'y'] if is_even(question) else ['no', 'n']
+
+
+def is_valid_answer(question, user_answer):
+    """Validate answer.
+
+    Parameters:
+        question: int
+        user_answer: str
+
+    Returns:
+        bool
+
+    """
+    return user_answer in correct_answer(question)
+
+
+def get_questions():
+    """Get numbers for game.
+
+    Returns:
+        list
+
+    """
+    return range(3)
+
+
+def start_game(user_name):
+    """Start game.
+
+    Parameters:
+        user_name: str
+
+    """
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for question in get_questions():
+        print('Question: {0}'.format(question))
+        user_answer = prompt.string()
+        print('Your answer: {0}'.format(user_answer))
+
+        if not is_valid_answer(question, user_answer):
+            error_message = "'{0}' is wrong answer ;(. Correct answer was '{1}'. Let's try again, {2}!"
+            print(error_message.format(user_answer, correct_answer(question), user_name))
+            return
+        print('Correct!')
+    print('Congratulations, {0}!'.format(user_name))
